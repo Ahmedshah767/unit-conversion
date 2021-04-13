08c5758f9a073b2f12998259ddfe9d07a5344d8f
Ahmedshah767 <81235297+Ahmedshah767@users.noreply.github.com>
2021-04-13 12:39:01 +0500

    Add files via upload

---
+++ my work.cpp
@@ -0,0 +1,8 @@
+/*input temperature in degree celcius, convert to degree fahrenheit*/
+#include <stdio.h>
+main ()
+{float temp;
+printf("input tempreture in degree celcius");
+scanf("%f",&temp);
+printf("equivalent tempreture in degree fahrenheit is %f",temp*1.8+32);
+}
